0fe48b21d9e249fd7ac1a5ff3815c6f4b0259e8d
plvo <talhatecim@gmail.com>
2025-02-19 18:43:19 +0100

    fix(componentsGRoup): read dir

--- src/lib/comp-groups.tsx
+++ src/lib/comp-groups.tsx
@@ -1,14 +1,15 @@
 import fs from 'fs';
+import path from 'path';
 
 export const getComponentsGroups = (): Array<string> => {
-  const registryFiles = fs.readdirSync('./public/r');
-  return Array.from(
-    new Set(
-      registryFiles.map((r) => {
-        return r.split('.')[0];
-      }),
-    ),
-  );
+  const dirPath = path.join(process.cwd(), 'public/r');
+
+  if (!fs.existsSync(dirPath)) {
+    return [];
+  }
+
+  const registryFiles = fs.readdirSync(dirPath);
+  return [...new Set(registryFiles.map((r) => r.split('.')[0]))];
 };
 
 export const getComponentsByGroup = (group: string): Array<string> => {
