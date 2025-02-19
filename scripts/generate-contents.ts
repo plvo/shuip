9f142667e00bb4b9b4c7c2bdbff57cddf8719b53
plvo <talhatecim@gmail.com>
2025-02-19 18:56:15 +0100

    wip(script): generate content

--- scripts/generate-contents.ts
+++ scripts/generate-contents.ts
@@ -4,8 +4,6 @@ import path from 'path';
 export const getComponentsGroups = (): Array<string> => {
   const dirPath = path.join(process.cwd(), 'public/r');
 
-  console.log(dirPath);
-
   if (!fs.existsSync(dirPath)) {
     return [];
   }
@@ -18,3 +16,24 @@ export const getComponentsByGroup = (group: string): Array<string> => {
   const registryFiles = fs.readdirSync('./public/r');
   return registryFiles.filter((r) => r.startsWith(`${group}.`));
 };
+
+async function main() {
+  const groups = getComponentsGroups();
+
+  const obj = {
+    ...groups.reduce(
+      (acc, group) => {
+        acc[group] = getComponentsByGroup(group);
+        return acc;
+      },
+      {} as { [key: string]: string[] },
+    ),
+  };
+
+  fs.writeFileSync('./src/content/comp-groups.json', JSON.stringify(obj, null, 2));
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
