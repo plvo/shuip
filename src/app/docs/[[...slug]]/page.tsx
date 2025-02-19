86a49397f6dbf7025d1a96913a983b52717307b5
plvo <talhatecim@gmail.com>
2025-02-19 18:18:42 +0100

    fix(docs/page): pageprops, async issues

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,27 +1,14 @@
-import { notFound } from 'next/navigation';
-
 import type { Metadata } from 'next';
-import Link from 'next/link';
+import { use } from 'react';
 
 interface DocPageProps {
-  params: {
+  params: Promise<{
     slug?: string[];
-  };
+  }>;
 }
 
-// async function getDocFromParams({ params }: DocPageProps) {
-//   const slug = params.slug?.join('/') || '';
-//   const doc = allDocs.find((doc) => doc.slugAsParams === slug);
-
-//   if (!doc) {
-//     return null;
-//   }
-
-//   return doc;
-// }
-
 export async function generateMetadata({ params }: DocPageProps): Promise<Metadata> {
-  if (!params.slug) {
+  if (!use(params).slug) {
     return {
       title: 'Documentation',
       description: 'Documentation for the project',
@@ -32,29 +19,14 @@ export async function generateMetadata({ params }: DocPageProps): Promise<Metada
     title: 'Documentation',
     description: 'Documentation for the project',
   };
-  //   const doc = await getDocFromParams({ params });
-
-  //   if (!doc) {
-  //     return {};
-  //   }
-
-  //   return {
-  //     title: doc.title,
-  //     description: doc.description,
-  //   };
 }
 
 export default async function Page({ params }: DocPageProps) {
-  if (!params.slug) {
+  if (!use(params).slug) {
+    <main className="relative py-6 lg:gap-10 lg:py-8 px-32 xl:grid xl:grid-cols-[1fr_300px]">
+      {JSON.stringify(params)}
+    </main>;
   }
-
-  //   const doc = await getDocFromParams({ params });
-
-  //   if (!doc) {
-  //     notFound();
-  //   }
-
-  //   const toc = await getTableOfContents(doc.body.raw);
 
   return (
     <main className="relative py-6 lg:gap-10 lg:py-8 px-32 xl:grid xl:grid-cols-[1fr_300px]">
