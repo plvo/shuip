1dc60b09d71281b895170bb52992505b8d5e2569
plvo <plvdev@proton.me>
2025-03-08 14:46:33 +0100

    feat(sidebar): fix bg and add drawer for mobile

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -172,26 +172,15 @@ function Sidebar({
 
   if (isMobile) {
     return (
-      <Sheet open={openMobile} onOpenChange={setOpenMobile} {...props}>
-        <SheetContent
-          data-sidebar="sidebar"
-          data-slot="sidebar"
-          data-mobile="true"
-          className="bg-sidebar/50 text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden"
-          style={
-            {
-              '--sidebar-width': SIDEBAR_WIDTH_MOBILE,
-            } as React.CSSProperties
-          }
-          side={side}
-        >
-          <SheetHeader className="sr-only">
-            <SheetTitle>Sidebar</SheetTitle>
-            <SheetDescription>Displays the mobile sidebar.</SheetDescription>
-          </SheetHeader>
-          <div className="flex h-full w-full flex-col">{children}</div>
-        </SheetContent>
-      </Sheet>
+      <Drawer open={openMobile} onOpenChange={setOpenMobile}>
+        <DrawerContent data-sidebar="sidebar" data-slot="sidebar" data-mobile="true">
+          <div className="mx-auto w-full max-w-sm overflow-y-auto">
+            <DrawerHeader></DrawerHeader>
+            {children}
+            <DrawerFooter></DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer>
     );
   }
 
