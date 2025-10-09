a5006ea3dfcb47463887d95f024ed935728f45df
main <plvdev@proton.me>
2025-10-09 22:12:00 +0200

    feat: add noChildrenProp option and update dependencies
    - Introduced the "noChildrenProp" option in biome.json for enhanced correctness checks.
    - Added "@radix-ui/react-separator" dependency in package.json and bun.lock for improved UI component functionality.
    - Refactored page.tsx to streamline form handling and improve code clarity by removing unused imports and simplifying the form submission process.

---
+++ src/app/tu/input-field.tsx
@@ -0,0 +1,35 @@
+import { Field, FieldDescription, FieldError, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { useFieldContext } from './hooks';
+
+export interface InputFieldProps {
+  label?: string;
+  description?: string;
+  inputProps?: React.ComponentProps<'input'>;
+  fieldProps?: React.ComponentProps<'div'> & { orientation?: 'vertical' | 'horizontal' | 'responsive' };
+}
+
+export function InputField({ label, description, fieldProps, inputProps }: InputFieldProps) {
+  const field = useFieldContext<string>();
+
+  const hasErrors = field.state.meta.errors.length > 0;
+  const errors = field.state.meta.errors.map((e) => e?.message).join(', ');
+
+  return (
+    <Field data-invalid={hasErrors} {...fieldProps}>
+      <FieldLabel>{label}</FieldLabel>
+      <Input
+        type='text'
+        id={field.name}
+        name={field.name}
+        value={field.state.value}
+        onChange={(e) => field.handleChange(e.target.value)}
+        onBlur={field.handleBlur}
+        aria-invalid={hasErrors}
+        {...inputProps}
+      />
+      {description && <FieldDescription>{description}</FieldDescription>}
+      {errors && <FieldError>{errors}</FieldError>}
+    </Field>
+  );
+}
--- src/app/tu/page.tsx
+++ src/app/tu/page.tsx
@@ -1,12 +1,10 @@
-/** biome-ignore-all lint/correctness/noChildrenProp: <explanation> */
 'use client';
 
-import { formOptions, mergeForm, useForm, useStore, useTransform } from '@tanstack/react-form';
-import { initialFormState } from '@tanstack/react-form/nextjs';
+import { formOptions, useStore } from '@tanstack/react-form';
 import { QueryClient, QueryClientProvider, useMutation, useQuery } from '@tanstack/react-query';
-import { useActionState } from 'react';
 import { z } from 'zod';
-import { getData, mutaction } from './actions';
+import { getData } from './actions';
+import { useAppForm } from './hooks';
 
 export const queryClient = new QueryClient();
 
@@ -26,64 +24,33 @@ function Content() {
     queryFn: getData,
   });
 
-  const [state, action] = useActionState(mutaction, initialFormState);
-
-  const form = useForm({
+  const form = useAppForm({
     ...opts,
-    transform: useTransform((baseForm) => mergeForm(baseForm, state!), [state]),
     validators: {
       onSubmit: schema,
     },
+    onSubmit: ({ value, meta }) => {
+      alert(JSON.stringify({ value, meta }));
+      form.reset();
+    },
   });
-
-  //   const mutation = useMutation({
-  //     mutationFn: action,
-  //   });
-
-  //   const form = useForm({
-  //     ...opts,
-  //     onSubmit: async ({ value, formApi }) => {
-  //       await mutation.mutateAsync(value);
-
-  //       await nameResult.refetch();
-
-  //       formApi.reset(value);
-  //     },
-
-  //     validators: {
-  //       onSubmit: schema,
-  //     },
-  //   });
-  //   const ddform = useFormRhf({});
 
   const values = useStore(form.store, (state) => state.values);
   const formState = useStore(form.store, (state) => state);
 
   return (
-    <div className='flex flex-col gap-4 justify-between w-screen'>
+    <div className='flex flex-col gap-4 mx-auto max-w-md justify-center items-center h-screen'>
       <form
-        action={action}
-        onSubmit={form.handleSubmit}
-        // onSubmit={(e) => {
-        //   e.preventDefault();
-        //   form.handleSubmit();
-        // }}
+        // action={action}
+        // onSubmit={form.handleSubmit}
+
+        onSubmit={(e) => {
+          e.preventDefault();
+          form.handleSubmit();
+        }}
       >
-        <form.Field
-          name='name'
-          children={(field) => {
-            return (
-              <input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(e) => field.handleChange(e.target.value)}
-                type='text'
-              />
-            );
-          }}
-        />
+        <form.AppField name='name' children={(f) => <f.InputField label='Name' />} />
+
         <form.Subscribe selector={(formState) => [formState.canSubmit, formState.isSubmitting]}>
           {([canSubmit, isSubmitting]) => (
             <button type='submit' disabled={!canSubmit}>
@@ -93,7 +60,7 @@ function Content() {
         </form.Subscribe>
       </form>
 
-      <pre>{JSON.stringify({ state, action, values, formState }, null, 2)}</pre>
+      <pre>{JSON.stringify({ values, errs: form.state.errors }, null, 2)}</pre>
     </div>
   );
 }
